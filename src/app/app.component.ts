465ae29e0a0ed7fc1f6621b9c5501d44bb67486c
zoomubik-collab <zoomubik@gmail.com>
2026-03-22 23:32:16 +0100

    Refactor AppComponent to use Angular dependency injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,1 +1,22 @@
-<ion-app></ion-app>
+import { Component, OnInit, inject } from '@angular/core';
+import { IonApp } from '@ionic/angular/standalone';
+import { Platform } from '@ionic/angular';
+import { NativeService } from './services/native.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  imports: [IonApp],
+})
+export class AppComponent implements OnInit {
+  private platform = inject(Platform);
+
+  // Se inyecta para inicializar features nativas (luego aquí pondremos Push remotas)
+  private nativeService = inject(NativeService);
+
+  async ngOnInit(): Promise<void> {
+    await this.platform.ready();
+    // No abrir InAppBrowser: Capacitor cargará https://zoomubik.com vía server.url
+  }
+}
