68d30830d46366a0d2fac62ffaa7eb1d7e4af2a4
zoomubik-collab <zoomubik@gmail.com>
2026-03-24 23:19:45 +0100

    Integrate Firebase Messaging in main.dart
    Initialize Firebase and set up background messaging.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-void main() => runApp(ZoomubikApp());
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  runApp(ZoomubikApp());
+}
 
 class ZoomubikApp extends StatelessWidget {
   @override
@@ -28,9 +40,28 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    _initFirebaseMessaging();
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..loadRequest(Uri.parse('https://www.zoomubik.com'));
+  }
+
+  Future<void> _initFirebaseMessaging() async {
+    // Solicitar permisos
+    await FirebaseMessaging.instance.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    // Obtener el token FCM (para debug)
+    String? token = await FirebaseMessaging.instance.getToken();
+    print('FCM Token: $token');
+
+    // Notificación recibida con la app en primer plano
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Mensaje recibido: ${message.notification?.title}');
+    });
   }
 
   @override
