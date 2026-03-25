b9397d341b577654e19a47f5c5a43ef9d93a8478
zoomubik-collab <zoomubik@gmail.com>
2026-03-25 11:32:05 +0100

    Integrate CLLocationManager in AppDelegate
    Added CoreLocation support to AppDelegate for location services.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,21 @@
 import Flutter
 import UIKit
+import CoreLocation
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, CLLocationManagerDelegate {
+  var locationManager: CLLocationManager?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    
+    locationManager = CLLocationManager()
+    locationManager?.delegate = self
+    locationManager?.requestWhenInUseAuthorization()
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
