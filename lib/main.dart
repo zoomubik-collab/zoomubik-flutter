f040ffce3112a1fdd8906175dce8458c7d9b9028
Zoomubik <soporte@zoomubik.com>
2026-03-27 00:23:50 +0100

    volver a webview_flutter - flutter_inappwebview daba pantalla blanca

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 ﻿import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   try {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
@@ -14,11 +12,6 @@ Future<void> main() async {
   } catch (e) {
     debugPrint('Error Firebase');
   }
-
-  if (!kDebugMode) {
-    await InAppWebViewController.setWebContentsDebuggingEnabled(false);
-  }
-
   runApp(ZoomubikApp());
 }
 
@@ -39,85 +32,23 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
-  late InAppWebViewController _webViewController;
-  final String _homeUrl = 'https://www.zoomubik.com';
-
-  final InAppWebViewSettings _settings = InAppWebViewSettings(
-    sharedCookiesEnabled: true,
-    incognito: false,
-    thirdPartyCookiesEnabled: true,
-    databaseEnabled: true,
-    domStorageEnabled: true,
-    cacheEnabled: true,
-    cacheMode: CacheMode.LOAD_CACHE_ELSE_NETWORK,
-    useShouldOverrideUrlLoading: true,
-    mediaPlaybackRequiresUserGesture: false,
-    useHybridComposition: true,
-    allowsInlineMediaPlayback: true,
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
-  );
+class _HomePageState extends State<HomePage> {
+  late final WebViewController _controller;
 
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addObserver(this);
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      _checkSessionAlive();
-    }
-  }
-
-  Future<void> _checkSessionAlive() async {
-    final cookies = await CookieManager.instance().getCookies(
-      url: WebUri(_homeUrl),
-    );
-    final isLoggedIn = cookies.any(
-      (c) => c.name.startsWith('wordpress_logged_in'),
-    );
-    debugPrint(isLoggedIn ? 'Sesion activa' : 'Sin sesion');
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1')
+      ..loadRequest(Uri.parse('https://www.zoomubik.com'));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: InAppWebView(
-          initialUrlRequest: URLRequest(
-            url: WebUri(_homeUrl),
-          ),
-          initialSettings: _settings,
-          onWebViewCreated: (controller) {
-            _webViewController = controller;
-            debugPrint('WebView creado');
-          },
-          onLoadStart: (controller, url) {
-            debugPrint('Cargando');
-          },
-          onLoadStop: (controller, url) async {
-            debugPrint('Pagina cargada');
-            await _checkSessionAlive();
-          },
-          onReceivedError: (controller, request, error) {
-            debugPrint('Error webview');
-          },
-          shouldOverrideUrlLoading: (controller, navigationAction) async {
-            final url = navigationAction.request.url?.toString() ?? '';
-            if (!url.startsWith('https://www.zoomubik.com')) {
-              return NavigationActionPolicy.CANCEL;
-            }
-            return NavigationActionPolicy.ALLOW;
-          },
-        ),
+        child: WebViewWidget(controller: _controller),
       ),
     );
   }
