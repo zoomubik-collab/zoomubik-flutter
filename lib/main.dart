8a83ccb2f49597b05d9e3da6e66609a08d1620c0
zoomubik-collab <zoomubik@gmail.com>
2026-03-28 02:32:10 +0100

    Refactor main and WebPage to use StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const ZoomubikApp());
 }
@@ -14,8 +14,15 @@ class ZoomubikApp extends StatelessWidget {
   }
 }
 
-class WebPage extends StatelessWidget {
+class WebPage extends StatefulWidget {
   const WebPage({super.key});
+  @override
+  State<WebPage> createState() => _WebPageState();
+}
+
+class _WebPageState extends State<WebPage> {
+  InAppWebViewController? _controller;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,8 +33,19 @@ class WebPage extends StatelessWidget {
           domStorageEnabled: true,
           databaseEnabled: true,
           cacheEnabled: true,
+          cacheMode: CacheMode.LOAD_DEFAULT,
           userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
         ),
+        onWebViewCreated: (controller) {
+          _controller = controller;
+        },
+        onLoadStop: (controller, url) async {
+          // Guardamos las cookies cada vez que carga una página
+          List<Cookie> cookies = await CookieManager.instance().getCookies(
+            url: WebUri('https://zoomubik.com'),
+          );
+          debugPrint('Cookies activas: ${cookies.length}');
+        },
       ),
     );
   }
