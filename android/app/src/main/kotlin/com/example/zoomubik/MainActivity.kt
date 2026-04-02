51ac49dd6bc2002f51b2e3a1535efa52c0de352f
zoomubik-collab <zoomubik@gmail.com>
2026-04-02 02:06:58 +0200

    Fix MainActivity package and embedding v2

--- android/app/src/main/kotlin/com/example/zoomubik/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.zoomubik
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/zoomubik/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.zoomubik.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
