8b3c7531858ee1326df38e15bdbabd8b6c33d1b7
zoomubik-collab <zoomubik@gmail.com>
2026-08-06 16:06:43 +0200

    Add Google Services plugin to settings.gradle.kts
    Added Google Services plugin for Firebase Analytics.

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,3 +1,4 @@
+
 pluginManagement {
     val flutterSdkPath =
         run {
@@ -21,6 +22,9 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    // Necesario para que google-services.json se procese en el build de Android
+    // y Firebase Analytics reporte (antes solo llegaban datos de iOS).
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 include(":app")
